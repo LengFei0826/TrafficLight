5d2195c30e98b9ea7de9f2e2eafa3eec7854d3a5
LengFei0826 <s1072767@gm.pu.edu.tw>
2020-06-05 14:46:24 +0800

    2020上機考版本1(何映龍)

--- app/src/main/java/tw/edu/pu/s1072767/trafficlight/MainActivity.java
+++ app/src/main/java/tw/edu/pu/s1072767/trafficlight/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
